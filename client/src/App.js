a3a4cd750402535d6cde2493b2fc7337a2d5c9e2
xavierprasetyo <xavierprasetyo89@gmail.com>
2020-05-20 21:39:25 +0700

    Add client folder back to git repo

---
+++ client/src/App.js
@@ -0,0 +1,27 @@
+import React, {Component} from 'react';
+import { Provider } from 'react-redux';
+import { Container } from 'reactstrap';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import {AppNavbar, Shoppinglist, ItemModal} from "./components";
+import store from './store';
+
+class App extends Component {
+  render() {
+    return (
+      <Provider store = {store}>
+        <div>
+          <AppNavbar/>
+          <Container>
+            <ItemModal/>
+            <Shoppinglist/>
+          </Container> 
+        </div>
+      </Provider>
+      
+    )
+  }
+}
+
+export default App;
---
+++ client/src/components/index.js
@@ -0,0 +1,3 @@
+export { default as AppNavbar } from './AppNavbar/AppNavbar';
+export { default as Shoppinglist } from './ShoppingList/Shoppinglist';
+export { default as ItemModal } from './ItemModal/ItemModal';
